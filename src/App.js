9f1fe8c752526ca56feee3409b676ecdb4975af3
Renato Silva <orenatodos@gmail.com>
2022-02-09 21:10:14 -0300

    feat: add routes in App file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyles } from './assets/styles/global'
 import { defaultTheme } from './assets/styles/themes/default'
 
+import { Header } from './components/Header'
+
+import { Routes } from './Routes'
+
+import * as S from './App.styles'
+
 export default function App () {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <h1>MyContacts</h1>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <S.Container>
+          <Header />
+          <Routes />
+        </S.Container>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
