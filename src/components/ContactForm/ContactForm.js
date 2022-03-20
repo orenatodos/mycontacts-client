a72e125d8ff23bb6eedcbcd9114b6fa5ebaa8887
Renato Silva <orenatodos@gmail.com>
2022-03-20 14:00:46 -0300

    feat: create useErrors hook

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { isEmailValid } from '../../utils/isEmailValid'
+import { useErrors } from '../../hooks/useErrors'
 
 import { Button } from '../Button'
 import { FormGroup } from '../FormGroup/FormGroup'
@@ -15,18 +16,19 @@ export const ContactForm = ({ buttonLabel }) => {
   const [email, setEmail] = useState('')
   const [phone, setPhone] = useState('')
   const [category, setCategory] = useState('')
-  const [errors, setErrors] = useState([])
+
+  const { setError, removeError, getErrorMessageByFieldName } = useErrors()
 
   function handleNameChange (event) {
     setName(event.target.value)
 
     if (!event.target.value) {
-      setErrors(prevState => [...prevState, {
+      setError({
         field: 'name',
         message: 'Nome é obrigatório.'
-      }])
+      })
     } else {
-      setErrors(prevState => prevState.filter(error => error.field !== 'name'))
+      removeError('name')
     }
   }
 
@@ -34,23 +36,13 @@ export const ContactForm = ({ buttonLabel }) => {
     setEmail(event.target.value)
 
     if (event.target.value && !isEmailValid(event.target.value)) {
-      const errorAlreadyExists = errors.find(error => error.field === 'email')
-
-      if (errorAlreadyExists) {
-        return
-      }
-
-      setErrors(prevState => [...prevState, {
+      setError({
         field: 'email',
         message: 'E-mail inválido.'
-      }])
+      })
     } else {
-      setErrors(prevState => prevState.filter(error => error.field !== 'email'))
+      removeError('email')
     }
-  }
-
-  function getErrorMessageByFieldName (fieldName) {
-    return errors.find(error => error.field === fieldName)?.message
   }
 
   function handleSubmit (event) {
